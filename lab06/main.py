0a2c749a5cc8a16f6b8651904ea02b5c5a097e5c
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-12-05 17:39:20 +0100

    bbb

--- lab06/main.py
+++ lab06/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def make_matrix(edges, vertices):
     matrix = []
     for i in range(vertices):
@@ -100,7 +103,6 @@ def gen_edges_to_add(pairs, graph, odds: list[int]):
         paths.remove(path)
         pairs.remove(pairs[min_index])
 
-
     return edges_to_add
 
 
@@ -125,6 +127,7 @@ def find_min_euler_cycle(edges, start):
         if stack[-1] == v:
             stack.pop()
     return ce
+
 
 # ---
 
@@ -172,12 +175,60 @@ def kruskal(edges, vertices):
     return mst
 
 
+def check_if_connected(edges, vertices: int):
+    visited = {}
+    for x in edges:
+        visited[x[0]] = False
+        visited[x[1]] = False
+
+    stack = [edges[0][0]]
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+            for edge in edges:
+                if edge[0] == v:
+                    stack.append(edge[1])
+                elif edge[1] == v:
+                    stack.append(edge[0])
+    return all(visited.items())
+
+
+def find_vertices_amount(edges):
+    stack = set()
+    for edge in edges:
+        stack.add(edge[0])
+        stack.add(edge[1])
+    return len(stack)
+
+
+def find_euler_cycle(edges, start=0):
+    print(edges)
+    ce = [start]
+    i = 2*len(edges)
+    while len(edges) > 0 and i > 0:
+        v = ce[-1]
+        i -= 1
+        print(ce)
+        for i in range(len(edges)):
+            if edges[i][0] == v or edges[i][1] == v:
+                edge = edges[i]
+                edges_coppy = edges.copy()
+                edges_coppy.pop(i)
+                if check_if_connected(edges, find_vertices_amount(edges_coppy)):
+                    ce.append(edge[0] if edge[1] == v else edge[1])
+                    edges.pop(i)
+                    break
+    return ce
+
+
 def main():
     # edges = [(0, 1, 2), (0, 7, 1), (1, 2, 2), (1, 8, 2), (2, 3, 3), (3, 4, 4), (3, 8, 3), (3, 9, 1),
     #          (4, 5, 3), (4, 9, 2), (5, 6, 5), (5, 9, 2), (6, 7, 1), (6, 8, 2), (6, 9, 4), (7, 8, 4)]
     # vertices = 10
 
-    edges = [(0, 1, 7), (0, 3, 5), (1, 2, 8), (1, 3, 9), (1, 4, 7), (2, 4, 5), (3, 4, 15), (3, 5, 6), (4, 5, 8), (4, 6, 9), (5, 6, 11)]
+    edges = [(0, 1, 7), (0, 3, 5), (1, 2, 8), (1, 3, 9), (1, 4, 7), (2, 4, 5), (3, 4, 15), (3, 5, 6), (4, 5, 8),
+             (4, 6, 9), (5, 6, 11)]
     vertices = 7
 
     mst = kruskal(edges, vertices)
@@ -186,7 +237,13 @@ def main():
     odds = find_odd(degrees)
     pairs = gen_pairs(odds)
     edges_to_add = gen_edges_to_add(pairs, graph, odds)
-    print(edges_to_add)
+    for x in edges_to_add:
+        mst.append(x)
+
+    random.shuffle(mst)
+
+    euler_cycle = find_euler_cycle(mst)
+    print(euler_cycle)
 
 
 if __name__ == '__main__':
